033080eb6abf9251d782f9fad58a026efbb0dc02
sergio <ser.sebastiani@gmail.com>
2023-12-09 23:17:54 +0000

    versione ok su pythonanywhare

--- pass/admin.py
+++ pass/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+@admin.register(models.Password)
+class PasswordAdmin(admin.ModelAdmin):
+    list_display = ['title','username']
+    #list_editable = ['title']
+    list_per_page = 10
+    #list_filter = ['title']
+    search_fields =  ['title']
+    #autocomplete_fields = ['tipo_prodotto']
--- pass/models.py
+++ pass/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
+
 class Password(models.Model):
-    title = models.CharField(max_length=255)
-    username = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
-    code = models.CharField(max_length=255)
-    link = models.URLField()
-    note = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, null=True, blank=True)
+    username = models.CharField(max_length=255, null=True, blank=True)
+    password = models.CharField(max_length=255, null=True, blank=True)
+    code = models.CharField(max_length=255, null=True, blank=True)
+    link = models.URLField(null=True, blank=True)
+    note = models.CharField(max_length=255,null=True, blank=True)
     
     def __str__(self) -> str:
         return self.title
